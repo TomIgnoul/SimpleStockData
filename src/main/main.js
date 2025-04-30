0e92680608bc4a1bc91e0b0d69db0ffe99252f10
tom.ignoul@student.ehb.be <tom.ignoul@student.ehb.be>
2025-04-30 05:54:02 +0200

    cleanup: deleted testdate

--- src/main/main.js
+++ src/main/main.js
@@ -6,22 +6,3 @@ import { renderChart } from "../utils/renderChart.js";
 import { initIntradayData } from "../init/initIntradayData.js";
 
 initIntradayData();
-//testdata
-// const dataA = [
-//   { time: "10:00", close: 150 },
-//   { time: "10:15", close: 155 },
-//   { time: "10:30", close: 160 },
-// ];
-
-// const dataB = [
-//   { time: "11:00", close: 140 },
-//   { time: "11:15", close: 135 },
-//   { time: "11:30", close: 138 },
-// ];
-
-//hier komt new Chart()
-// renderChart(dataA);
-
-// document.getElementById("switchChart").addEventListener("click", () => {
-//   renderChart(dataB);
-// });
