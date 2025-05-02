7c431cc712508f54d2210ecc92f15cb5600240f1
TomIgnoul <tom.ignoul@student.ehb.be>
2025-05-02 16:08:04 +0200

    Feature: interval slider

--- src/main/main.js
+++ src/main/main.js
@@ -2,11 +2,20 @@
 
 import { Chart } from "chart.js/auto";
 import { renderChart } from "../utils/renderChart.js";
-import { initIntradayData } from "../init/initIntradayData.js";
+import { intradayData } from "../init/intradayData.js";
 import { searchSymbol } from "../api/searchSymbolAPI.js";
 
-initIntradayData();
+
 
 searchSymbol("tesla").then((resultaten) => {
   console.log(resultaten);
 });
+const stringValues = ["1min", "5min", "15min", "30min", "60min"];
+const slider = document.getElementById("slider");
+const output = document.getElementById("slider-value");
+intradayData("AAPL", stringValues[slider.value]);
+slider.addEventListener("input", () => {
+  console.log(stringValues[slider.value]);
+  intradayData("AAPL", stringValues[slider.value]);
+});
+
