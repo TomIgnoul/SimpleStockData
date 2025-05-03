1b1bd0dd6c2c854ead6122a37215f42a9a350b1c
Tom Ignoul <tom.ignoul@student.ehb.be>
2025-05-03 08:08:03 +0200

    Cleaned up redunded code

--- src/api/stockRepository.js
+++ src/api/stockRepository.js
@@ -26,42 +26,4 @@ export class StockRepository {
       return [];
     }
   }
-
-  //
-  // const key = `Time Series (${interval})`;
-
-  // if (data["Error Message"] || data["Note"]) {
-  //   throw new Error(data["Error Message"] || data["Note"]);
-  // }
-
-  // if (!data[key]) {
-  //   console.warn(`No data found for ${symbol} with interval ${interval}.`);
-  //   return { [key]: {} };
-  // }
-
-  // console.log(`Full intraday data for ${symbol}:`, data);
-  // return data;
-
-  // async searchSymbol(keyword) {
-  //   const url = `${
-  //     this.baseUrl
-  //   }?function=SYMBOL_SEARCH&keywords=${encodeURIComponent(keyword)}&apikey=${
-  //     this.apiKey
-  //   }`;
-
-  //   try {
-  //     const response = await fetch(url);
-  //     if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
-
-  //     const data = await response.json();
-  //     if (data["Error Message"] || data["Note"]) {
-  //       throw new Error(data["Error Message"] || data["Note"]);
-  //     }
-
-  //     return data.bestMatches || [];
-  //   } catch (error) {
-  //     console.error("Symbol search failed:", error.message);
-  //     return [];
-  //   }
-  // }
 }
--- src/utils/transformIntradayData.js
+++ src/utils/transformIntradayData.js
@@ -14,20 +14,3 @@ export function transformIntradayData(dataArray) {
     volume: parseInt(entry.Volume, 10),
   }));
 }
-
-// export function transformIntradayData(intradayData, interval) {
-//   const seriesKey = `Time Series (${interval})`;
-//   const raw = intradayData[seriesKey];
-//   if (!raw) {
-//     console.error(`kon '${seriesKey}' niet vinden in Response`, intradayData);
-//     return [];
-//   }
-//   return Object.entries(raw).map(([time, vals]) => ({
-//     time,
-//     open: parseFloat(vals["1. open"]),
-//     high: parseFloat(vals["2. high"]),
-//     low: parseFloat(vals["3. low"]),
-//     close: parseFloat(vals["4. close"]),
-//     volume: parseInt(vals["5. volume"], 10),
-//   }));
-// }
